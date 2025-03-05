f19591079f4426c1f32e2b0b62e04cd4213e4011
Siddartha Kommu <kommusiddartha001@gmail.com>
2025-03-05 08:31:35 +0530

    done w some day 3 tasks

--- day3/task3.py
+++ day3/task3.py
@@ -1,4 +1,4 @@
 text = "Hello World!"
 s={}
-s={i.lower() for i in text if i not in s and i.isalpha()}
+s={i.lower() for i in text if i.isalpha()}
 print(s)
--- day3/task5.py
+++ day3/task5.py
@@ -1,7 +1,7 @@
 m = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-#ma=[[rw[i] for rw in matrix] for i in range(len(matrix[0]))]
-#print(ma)
+# ma=[[rw[i] for rw in matrix] for i in range(len(matrix[0]))]
+# print(ma)
 
 for i in range(len(m)):
     for j in range(len(m[0])):
